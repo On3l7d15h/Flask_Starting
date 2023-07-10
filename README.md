f835b70a6e8f8621b171e20d9112932cefd9eee3
On3l7d15h <Onelldish@hotmail.com>
2023-07-09 21:04:48 -0300

    readme and pictures added! Also, api_db_env done.

---
+++ README.md
@@ -0,0 +1,51 @@
+# FLASK - Starting Repository | Repositorio Inicial (Bilingual)
+
+
+😀 **¡Hola!** 😀
+Por aquí les muestro unos mini proyectos iniciales hechos con Flask, el cual es un framework web de python.
+
+
+😀 Greetings! 😀
+Now, I show you some mini projects using Flask, which is a python web development backend framework.
+
+## 🖼 Screenshot / Capturas de Pantalla 🖼
+
+### Student Register (No Style) | Registro de Estudiantes (Sin Estilo)
+![](/assets/images/student-register.png)
+![](/assets/images/student-register2.png)
+![](/assets/images/student-register3.png)
+
+### Simple Layout Web App | Estructura de Aplicación Web simple.
+![](/assets/images/flaska.png)
+![](/assets/images/flaska2.png)
+![](/assets/images/flaska3.png)
+
+### Meal App | Aplicación para registrar comidas
+![](/assets/images/meals.png)
+![](/assets/images/meals2.png)
+![](/assets/images/meals3.png)
+
+
+## ✨ Features / Características 🌟
+
+Aunque los proyectos mostrados aquí son solamente para familiarizarse con el framework... algunos de ellos son funcionales. 🌟✨
+
+Even though the displayed projects are just for practice... actually, some of then works. 🌟✨
+
+
+## 💻 Technologies / Tecnologías 💻
+Las Tecnología usada para este 👷proyecto👷 es:
+
+``` Python
+tecnologia = ["Python", "Html", "CSS", "JavaScript", "Sqlite3", "TailwindCSS"];
+```
+
+The technology used to make this 👷project👷 reality is:
+
+``` Python
+technology = ["Python", "Html", "CSS", "JavaScript", "Sqlite3", "TailwindCSS"];
+```
+
+## 👣 Footer Page / Pie de Página 👣
+
+🌟✨Desarrollado por On3l7d15h. / Developed By On3l7d15h.✨🌟
--- practicing/api_db_env/main.py
+++ practicing/api_db_env/main.py
@@ -22,7 +22,6 @@ def get_users_route():
 
             cur = conn.execute("SELECT * FROM users;");
             ans = cur.fetchall();
-
     except:
         conn.rollback();
         print("Error... please check your code.")
@@ -32,6 +31,82 @@ def get_users_route():
 
     return ans;
 
+@app.route("/api/get/users/<id>")
+def get_specific_user(id):
+     
+    ans = [];
+
+    try:
+        with sql.connect("database.db") as conn:
+            cur = conn.execute("SELECT * FROM users WHERE id = ?", (id));
+            ans = cur.fetchall();
+    except:
+        conn.rollback();
+        print("Error... please check your code.")
+    finally:
+        conn.close()
+
+    if len(ans) == 0:
+        return "There is not a register with that id";
+    
+    return ans;
+
+@app.route("/api/get/users/<id>/update", methods=['POST'])
+def update_specific_user(id: int):
+    utu = flask.request.get_json();
+    was_updated = False;
+
+    try:
+        with sql.connect("database.db") as conn:
+            conn.execute("UPDATE users SET name = ?, last = ?, title = ? WHERE id = ?;", (utu['name'], utu['last'], utu['title'], id))
+        was_updated = True;
+    except:
+        conn.rollback();
+        print("Error... please check your code.")
+    finally:
+        conn.close();
+
+    return "Updated Successfully" if was_updated else "Oops! Try again later..."
+
+@app.route("/api/get/users/<id>/delete", methods=['POST'])
+def delete_specific_user(id: int):
+    utd = flask.request.get_json();
+    was_deleted = False;
+
+    if utd["answer"] == 'NO':
+        return "You decided to not delete this register!"
+
+    try:
+        with sql.connect("database.db") as conn:    
+            conn.execute("DELETE FROM users WHERE id = ?;", (id))
+            conn.commit();
+            was_deleted = True;
+    except:
+        conn.rollback();
+        print("Error...  please check your code.")
+    finally:
+        conn.close();
+
+    return "Deleted Successfully" if was_deleted else "Oops! The server have had problems with your request!";
+
+@app.route("/api/post/newuser", methods=["POST"])
+def create_user_route():
+    new_user = flask.request.get_json();
+    was_created = False;
+
+    try:
+        with sql.connect("database.db") as conn:
+            conn.execute("INSERT INTO users(name, last, title) VALUES(?, ?, ?)", (new_user["name"], new_user["last"], new_user["title"]));
+            conn.commit();
+            was_created = True;
+    except:
+        conn.rollback();
+        print("Error... please check your code.")
+    finally:
+        conn.close();
+    
+    return "Created Successfully" if was_created else "Oops! try again later, we had some troubles..."
+
 # 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=7000)
